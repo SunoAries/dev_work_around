c5eac320d650b7c8463f674d7187be4c16ba1e8d
xiao_ubuntu <a@x.com>
2019-04-14 08:32:02 -0700

    add nvm

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -2,3 +2,5 @@ sudo apt-get install git
 # install zsh && oh-my-zsh
 apt install zsh 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# install nvm  
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
