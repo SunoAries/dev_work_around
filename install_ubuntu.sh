b0542a5ef7fc93ed81e5c6bb311c268cc359858e
SunoAries <476252630@qq.com>
2019-06-19 14:41:10 +0800

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y git python3 zsh curl wget 
+sudo apt-get install -y git python3 zsh curl wget tig
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # install yarn
@@ -6,3 +6,7 @@ curl -o- -L https://yarnpkg.com/install.sh | bash -s
 # install nvm  
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 
+
+ssh-keygen -t ras
+
+cat ~/.ssh/id_rsa.pub
